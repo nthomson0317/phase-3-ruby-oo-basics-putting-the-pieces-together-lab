d7f3913dda058a365438d2d0d6d612708cadd11e
Nicholas Thomson <nicholasgraythomson@gmail.com>
2021-06-07 21:30:36 -0400

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,16 @@
+class Book
+    attr_accessor :title, :author, :genre, :page_count 
+    # _
+
+
+    def initialize(title)
+        @title = title
+    end
+
+    def turn_page 
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
